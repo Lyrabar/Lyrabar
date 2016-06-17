2b6ed8c125b4d62a9c10b23184cf9ace73ab1ca5
Lyrabar <lyra2bar@gmail.com>
2016-06-17 20:52:44 +0300

    Update iconset creation script

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -2,8 +2,7 @@
 # create multiresolution windows icon
 ICON_SRC=../../src/qt/res/icons/lyrabar.png
 ICON_DST=../../src/qt/res/icons/lyrabar.ico
-convert ${ICON_SRC} -resize 16x16 vertcoin-16.png
-convert ${ICON_SRC} -resize 32x32 vertcoin-32.png
-convert ${ICON_SRC} -resize 48x48 vertcoin-48.png
-convert vertcoin-16.png vertcoin-32.png vertcoin-48.png ${ICON_DST}
-
+convert ${ICON_SRC} -resize 16x16 lyrabar-16.png
+convert ${ICON_SRC} -resize 32x32 lyrabar-32.png
+convert ${ICON_SRC} -resize 48x48 lyrabar-48.png
+convert lyrabar-16.png lyrabar-32.png lyrabar-48.png ${ICON_DST}
